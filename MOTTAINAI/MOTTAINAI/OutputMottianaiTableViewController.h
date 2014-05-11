92eb82c30aa5304bed9758a7b3d01fd6cbe0a636
Kouki Saito <dan.addr.skd@gmail.com>
2014-05-11 13:16:26 +0900

    Add new class for output mottainai data
    The new class name is OutputMottainaiTableViewController.

---
+++ MOTTAINAI/MOTTAINAI/OutputMottianaiTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  OutputMottianaiTableViewController.h
+//  MOTTAINAI
+//
+//  Created by Kouki Saito on 2014/05/11.
+//  Copyright (c) 2014年 Kouki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OutputMottianaiTableViewController : UITableViewController
+
+@end
